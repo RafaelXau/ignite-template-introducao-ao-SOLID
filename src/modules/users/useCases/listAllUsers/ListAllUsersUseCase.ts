c2fc8a9298e4c6112b9c4503859c8ccf8746a5a5
Rafael Honório <rafaelxhs@gmail.com>
2022-01-04 18:23:14 -0300

    Desafio Concluído

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists) {
+      throw new Error("User not found!");
+    }
+
+    if (userExists.admin !== true) {
+      throw new Error("User must be an admin to see a list of users.");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,16 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    const user = {};
+
+    try {
+      Object.assign(user, this.turnUserAdminUseCase.execute({ user_id }));
+    } catch (error) {
+      return response.status(404).json({ error });
+    }
+
+    return response.status(201).json(user);
   }
 }
 
